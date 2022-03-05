7ef538433f1724b33c29fa822ec41abd37734bd1
yoav-ro <89576513+yoav-ro@users.noreply.github.com>
2022-03-05 20:00:52 +0200

    basic tile component
    contains clickable junctions and borders

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import HexagonBoard from './components/hexagonsBoard';
-import ResourceTile from './components/resourceTile';
+// import ResourceTile from './components/resourceTile';
 
 function App() {
   return (
     <div>
       {/* <ResourceTile /> */}
       <HexagonBoard />
+      {/* <ResourceTile row={0} cell={1}/> */}
     </div>
   );
 }
+
+
 
 export default App;
--- client/src/components/hexagonsBoard.jsx
+++ client/src/components/hexagonsBoard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ResourceTile from "./resourceTile";
+import ResourceTile from "./resourceTile/resourceTile";
 import { getRowsData } from "../utils/boardHelper";
 import "./styles/hexagon.css"
 
@@ -7,33 +7,38 @@ function HexagonBoard() {
     const rowsData = getRowsData();
     console.log(rowsData)
 
+    const initX = 151.5;
+    const initY = 48.5;
+
     return (
-        <div>
+        <div id="board">
+            {/* <svg height={1080} width={1920}> */}
             <div id="row0" className="row">
                 {rowsData.row0.map((type, key) => {
-                    return <ResourceTile key={key} resourceType={type} />
+                    return <ResourceTile key={key} resourceType={type} row={1} cell={key + 1} />
                 })}
             </div><br />
             <div id="row1" className="row">
                 {rowsData.row1.map((type, key) => {
-                    return <ResourceTile key={key} resourceType={type} />
+                    return <ResourceTile key={key} resourceType={type} row={2} cell={key + 1} />
                 })}
             </div><br />
             <div id="row2" className="row">
                 {rowsData.row2.map((type, key) => {
-                    return <ResourceTile key={key} resourceType={type} />
+                    return <ResourceTile key={key} resourceType={type} row={3} cell={key + 1} />
                 })}
             </div><br />
             <div id="row3" className="row">
                 {rowsData.row3.map((type, key) => {
-                    return <ResourceTile key={key} resourceType={type} />
+                    return <ResourceTile key={key} resourceType={type} row={4} cell={key + 1} />
                 })}
             </div><br />
             <div id="row5" className="row">
                 {rowsData.row4.map((type, key) => {
-                    return <ResourceTile key={key} resourceType={type} />
+                    return <ResourceTile key={key} resourceType={type} row={5} cell={key + 1} />
                 })}
             </div><br />
+            {/* </svg> */}
         </div>
     )
 
