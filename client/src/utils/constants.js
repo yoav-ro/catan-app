f958f691a631689ef5c819c0b3480e66223d5c70
yoav-ro <89576513+yoav-ro@users.noreply.github.com>
2022-03-06 14:10:21 +0200

    tiles data
    shows color, number bugged

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,26 +1,43 @@
-export const resources = {
+export const resourcesTypes = {
     WOOD: {
         name: "wood",
-        ref: "http://catan-map.surge.sh/static/media/brick.90e15df7.png"
+        ref: "http://catan-map.surge.sh/static/media/brick.90e15df7.png",
+        color: "#dbe36b",
     },
     BRICK: {
         name: "brick",
-        ref: "http://catan-map.surge.sh/static/media/brick.90e15df7.png"
+        ref: "http://catan-map.surge.sh/static/media/brick.90e15df7.png",
+        color: "brown",
     },
     WHEAT: {
         name: "wheat",
-        ref: "http://catan-map.surge.sh/static/media/brick.90e15df7.png"
+        ref: "http://catan-map.surge.sh/static/media/brick.90e15df7.png",
+        color: "#255928",
     },
     SHEEP: {
         name: "sheep",
-        ref: "http://catan-map.surge.sh/static/media/brick.90e15df7.png"
+        ref: "http://catan-map.surge.sh/static/media/brick.90e15df7.png",
+        color: "#b8f2bc"
     },
     IRON: {
         name: "iron",
-        ref: "http://catan-map.surge.sh/static/media/brick.90e15df7.png"
+        ref: "http://catan-map.surge.sh/static/media/brick.90e15df7.png",
+        color: "#949995",
     },
     DESERT: {
         name: "desert",
-        ref: "http://catan-map.surge.sh/static/media/brick.90e15df7.png"
+        ref: "http://catan-map.surge.sh/static/media/brick.90e15df7.png",
+        color: "#f2eeb8",
     },
 }
+
+export const numbersArr = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12];
+
+export const resourcesArr = [
+    resourcesTypes.BRICK, resourcesTypes.BRICK, resourcesTypes.BRICK,
+    resourcesTypes.WOOD, resourcesTypes.WOOD, resourcesTypes.WOOD, resourcesTypes.WOOD,
+    resourcesTypes.IRON, resourcesTypes.IRON, resourcesTypes.IRON,
+    resourcesTypes.SHEEP, resourcesTypes.SHEEP, resourcesTypes.SHEEP, resourcesTypes.SHEEP,
+    resourcesTypes.WHEAT, resourcesTypes.WHEAT, resourcesTypes.WHEAT, resourcesTypes.WHEAT,
+    resourcesTypes.DESERT,
+]
