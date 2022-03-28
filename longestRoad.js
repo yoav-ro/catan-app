235c8d6569ba2e0d7809aa3d6aff4cd94d49aa0c
yoav-ro <89576513+yoav-ro@users.noreply.github.com>
2022-03-28 23:25:23 +0300

    longest road

--- longestRoad.js
+++ longestRoad.js
@@ -7,72 +7,87 @@ const getRoadObj = (startX, startY, endX, endY, color) => {
         color: color,
     }
 }
-
 const roads = [
-    getRoadObj(363.73066958946424, 35, 303.10889132455355, 0, "blue"),
-    getRoadObj(363.73066958946424, 35, 363.7306695894643, 105, "blue"),
-    getRoadObj(363.73066958946424, 105, 303.10889132455355, 140, "blue"),
-    getRoadObj(303.10889132455355, 140, 303.10889132455355, 210, "blue"),
-    getRoadObj(363.73066958946424, 245, 303.10889132455355, 210, "blue"),
-    getRoadObj(303.10889132455355, 210, 242.48711305964284, 245, "blue"),
-    getRoadObj(242.48711305964284, 245, 242.48711305964284, 315, "blue"),
-    getRoadObj(424.35244785437493, 210, 363.73066958946424, 245, "red"),
-    getRoadObj(363.73066958946424, 245, 363.73066958946424, 315, "red"),
+    getRoadObj(364.73066958946424, 35, 303.10889132455355, 0, "blue"), //0
+    getRoadObj(364.73066958946424, 35, 364.73066958946424, 105, "blue"), //1
+    getRoadObj(364.73066958946424, 105, 303.10889132455355, 140, "blue"), //2
+    getRoadObj(303.10889132455355, 140, 303.10889132455355, 210, "blue"), //3
+    getRoadObj(364.73066958946424, 245, 303.10889132455355, 210, "blue"), //4
+    getRoadObj(303.10889132455355, 210, 242.48711305964284, 245, "blue"), //5
+    getRoadObj(242.48711305964284, 245, 242.48711305964284, 315, "blue"), //6
+    getRoadObj(424.35244785437493, 210, 364.73066958946424, 245, "red"), //7
+    getRoadObj(364.73066958946424, 245, 364.73066958946424, 315, "red"), //8
 ]
-// console.log(roads)
-findLongestRoad(roads);
+//longest should be (road indexes): [0,1,2,3,5,6]
 
-function findLongestRoad(roads) {
-    const test = theRecurtionThing(roads);
-    // console.log(test);
+let long = calcLongestRoad();
+// console.log(long);
+
+function calcLongestRoad() {
+    let longestRoad = [];
+    roads.forEach(road => {
+        const longestByMyRoad = getLongestFromSegment([road]);
+        console.log("new longest?")
+        console.log(longestByMyRoad)
+        if (longestByMyRoad.length > longestRoad.length) {
+            longestRoad = longestByMyRoad;
+        }
+    });
+    return longestRoad;
 }
 
-function theRecurtionThing(currRoadSeq) {
-    const nextSeq = findConnectedRoads(currRoadSeq);
-    if (nextSeq.length === 0) {
-        return currRoadSeq;
+function getLongestFromSegment(currSeq) {
+    const nextSegments = checkSegmentNeighbor(currSeq);
+    if (nextSegments.length === 0) {
+        return currSeq;
     }
-
-    nextSeq.forEach(newNode => {
-        const newSeq = currRoadSeq.slice().push(newNode);
-        console.log(newNode);
-        // console.log(newSeq);
-        return theRecurtionThing(newSeq);
+    nextSegments.forEach(nextSegment => {
+        const nextProcess = [...currSeq, nextSegment];
+        return getLongestFromSegment(nextProcess);
     })
 }
 
-function findConnectedRoads(roadSeq) {
-    console.log(roadSeq)
+function checkSegmentNeighbor(roadSeq) {
     const lastRoad = roadSeq[roadSeq.length - 1];
-    // console.log(lastRoad)
-    const { color, startX, startY, endX, endY } = lastRoad;
     const connectedRoads = [];
     roads.forEach(road => {
-        if (road.color === color && !roadSeq.includes[road]) {
-            if (road.startX === startX && road.startY === startY && road.endX !== endX && road.endX !== endY) {
-                if (validateRoadSequence(road.startX, road.startY)) {
-                    connectedRoads.push(road);
-                }
-            }
-            if (road.startX !== startX && road.startY !== startY && road.endX === endX && road.endX === endY) {
-                if (validateRoadSequence(road.endX, road.endY)) {
-                    connectedRoads.push(road);
-                }
-            }
-            if (road.startX === endX && road.startY === endY && road.endX !== startX && road.endX !== startY) {
-                if (validateRoadSequence(road.startX, road.startY)) {
-                    connectedRoads.push(road);
-                }
-            }
-            if (road.endX === startX && road.endY === startY && road.startX !== endX && road.startY !== endY) {
-                if (validateRoadSequence(road.endX, road.endY)) {
-                    connectedRoads.push(road);
-                }
+        if (!roadSeq.includes(road)) {
+            if (areRoadsConnected(lastRoad, road)) {
+                connectedRoads.push(road);
             }
         }
     })
     return connectedRoads;
 }
+
+function areRoadsConnected(roadA, roadB) {
+    const { color: colorA, startX: startXA, startY: startYA, endX: endXA, endY: endYA } = roadA;
+    const { color: colorB, startX: startXB, startY: startYB, endX: endXB, endY: endYB } = roadB;
+    if (colorA === colorB) {
+        if ((startXA === startXB && startYA === startYB) && (endXA !== endXB || endYA !== endYB)) {
+            if (validateRoadSequence(startXA, startYA)) {
+                return true;
+            }
+        }
+        if ((startXA !== startXB || startYA !== startYB) && (endXA === endXB && endYA === endYB)) {
+            if (validateRoadSequence(endXA, endYA)) {
+                return true;
+            }
+        }
+        if ((startXA === endXB && startYA === endYB) && (endXA !== startXB || endYA !== startYB)) {
+            if (validateRoadSequence(startXA, startYA)) {
+                return true;
+            }
+        }
+        if ((startXA !== endXB || startYA !== endYB) && (endXA === startXB && endYA === startYB)) {
+            if (validateRoadSequence(endXA, endYA)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 
 function validateRoadSequence(player, x, y) {
     // if (getJunctionStatus(x, y).player !== player) {
